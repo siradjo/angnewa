077341bd9f13dd45f06e93adb83802ddd979398b
Siradjo Balde <siradjo90@gmail.com>
2025-08-07 13:59:53 +0200

    Ignore .env file

--- angnewa/settings_production.py
+++ angnewa/settings_production.py
@@ -9,7 +9,9 @@ environ.Env.read_env(BASE_DIR / ".env")
 
 SECRET_KEY = env('SECRET_KEY')
 DEBUG = env.bool('DEBUG', default=False)
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['127.0.0.1', 'localhost'])
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=["127.0.0.1", "localhost"])
+
+
 
 
 
