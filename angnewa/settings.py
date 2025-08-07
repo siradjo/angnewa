01513a97440e69c68fdd21105350fa10c57db12a
Siradjo Balde <siradjo90@gmail.com>
2025-08-07 07:08:29 +0200

    Fix ALLOWED_HOSTS pour Render

--- angnewa/settings.py
+++ angnewa/settings.py
@@ -19,7 +19,8 @@ DEBUG = env('DEBUG')
 # ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=[])
 # ALLOWED_HOSTS = ['angnewa.onrender.com', 'localhost', '127.0.0.1']
 #ALLOWED_HOSTS = ['*']
-ALLOWED_HOSTS = ['angnewa.onrender.com', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['angnewa.onrender.com', 'localhost', '127.0.0.1']
+
 
 
 
