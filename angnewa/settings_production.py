b6c25f4a198d96b71ce90c4f2867534f3c643d4d
Siradjo Balde <siradjo90@gmail.com>
2025-08-07 14:59:04 +0200

    Ignore .env file

--- angnewa/settings_production.py
+++ angnewa/settings_production.py
@@ -12,7 +12,9 @@ environ.Env.read_env(BASE_DIR / ".env")
 
 SECRET_KEY = env('SECRET_KEY')
 DEBUG = env.bool('DEBUG', default=False)
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['angnewa.onrender.com', '127.0.0.1', 'localhost']
+
+
 
 INSTALLED_APPS = [
     'django.contrib.admin',
