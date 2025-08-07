1ab8c7a76cc68c9605e02c3ef5b5b3107be5f8cd
Siradjo Balde <siradjo90@gmail.com>
2025-08-07 08:00:34 +0200

    Fix ALLOWED_HOSTS + Ajout Procfile et wsgi.py

--- angnewa/settings.py
+++ angnewa/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
 
 # Middlewares
 MIDDLEWARE = [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
